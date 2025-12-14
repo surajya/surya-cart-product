61ef945399261083f74a52285b48b9a104726893
surajvns1571999 <surajvns1571999@gmail.com>
2025-12-14 09:44:36 +0530

    suryacart setup

--- src/main/java/spring/dao/ProductDao.java
+++ src/main/java/spring/dao/ProductDao.java
@@ -2,35 +2,34 @@ package spring.dao;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
 
+import jakarta.transaction.Transactional;
 import spring.Modal.Product;
 
 @Component
 public class ProductDao {
 	@Autowired
 	private HibernateTemplate hibernatetemplate;
-	
+
 	@Transactional
-	public void CreateProduct(Product product) {
+	public void createProduct(Product product) {
 		this.hibernatetemplate.saveOrUpdate(product);
 	}
-	
-	public List<Product> getAllProduct(){
-		List<Product> All_Product = this.hibernatetemplate.loadAll(Product.class);
-		return All_Product;
+
+	public List<Product> getAllProduct() {
+		List<Product> products = this.hibernatetemplate.loadAll(Product.class);
+		return products;
 	}
-	
+
 	@Transactional
 	public void deleteProduct(int pid) {
 		Product p = this.hibernatetemplate.get(Product.class, pid);
 		this.hibernatetemplate.delete(p);
 	}
-	
+
 	public Product getProduct(int pid) {
 		return this.hibernatetemplate.get(Product.class, pid);
 	}
--- src/main/java/spring/mainController/Handler.java
+++ src/main/java/spring/mainController/Handler.java
@@ -2,10 +2,7 @@ package spring.mainController;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.server.RequestPath;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,57 +11,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
 
+import jakarta.servlet.http.HttpServletRequest;
 import spring.Modal.Product;
 import spring.dao.ProductDao;
 
 @Controller
 public class Handler {
-	
+
 	@Autowired
 	private ProductDao pd;
-	
+
 	@RequestMapping("/")
 	public String home(Model m) {
 		List<Product> allProduct = pd.getAllProduct();
 		m.addAttribute("products", allProduct);
-		
+
 		return "/index";
 	}
-	
+
 	@RequestMapping("/addProduct")
-	public String addProduct(Model m ) {
-		m.addAttribute("title","Add Product");
+	public String addProduct(Model m) {
+		m.addAttribute("title", "Add Product");
 		return "add_product_form";
 	}
-	
+
 	@RequestMapping("/delete/{pid}")
-	public RedirectView deleteProduct(@PathVariable("pid") int id ,HttpServletRequest request) {
-		
+	public RedirectView deleteProduct(@PathVariable("pid") int id,
+			HttpServletRequest request) {
+
 		pd.deleteProduct(id);
-		RedirectView rv=new RedirectView();
-		rv.setUrl(request.getContextPath()+"/");
+		RedirectView rv = new RedirectView();
+		rv.setUrl(request.getContextPath() + "/");
 		return rv;
 	}
-	
-	@RequestMapping(value = "handleProductForm", method=RequestMethod.POST)
-	public RedirectView handleForm(@ModelAttribute Product product, HttpServletRequest request) {
+
+	@RequestMapping(value = "handleProductForm", method = RequestMethod.POST)
+	public RedirectView handleForm(@ModelAttribute Product product,
+			HttpServletRequest request) {
 		System.out.println(product);
-		pd.CreateProduct(product);
-		RedirectView rv=new RedirectView();
-		rv.setUrl(request.getContextPath()+"/");
+		pd.createProduct(product);
+		RedirectView rv = new RedirectView();
+		rv.setUrl(request.getContextPath() + "/");
 		return rv;
 	}
-	
-	@RequestMapping(value="/update/{pid}")
-	public String updateElement(@PathVariable("pid") int id,Model m) {
+
+	@RequestMapping(value = "/update/{pid}")
+	public String updateElement(@PathVariable("pid") int id, Model m) {
 		Product product = this.pd.getProduct(id);
 		m.addAttribute("product", product);
 		return "update_product";
 	}
-	
-//	@RequestMapping(value="/udpateValue", method=RequestMethod.POST)
-//	public RedirectView udpdateEl() {
-//		
-//	}
-}   
 
+	// @RequestMapping(value="/udpateValue", method=RequestMethod.POST)
+	// public RedirectView udpdateEl() {
+	//
+	// }
+}
